a1ef64a41032324e6141daa37c3fe02c3bb1bcd3
Mikel Matticoli <10008957@sbschools.org>
2016-09-16 07:30:10 -0500

    Added cardboard support

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	void Update () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        float my = Input.GetAxis("Vertical");
+        float mx = Input.GetAxis("Horizontal");
+
+        rb.AddForce(new Vector3(mx*speed, 0.0f, my*speed));
+    }
+}
