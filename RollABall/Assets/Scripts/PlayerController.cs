f8ae05d15817df91846949020093c8f07b9d3ccb
Mikel Matticoli <10008957@sbschools.org>
2016-09-20 12:53:50 -0400

    Added pickup prefab

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     Rigidbody rb;
+    private Vector3 rinit;
     public float speed;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        rinit = transform.position;
     }
 
 	void Update () {
@@ -21,5 +23,15 @@ public class PlayerController : MonoBehaviour {
         float mx = Input.GetAxis("Horizontal");
 
         rb.AddForce(new Vector3(mx*speed, 0.0f, my*speed));
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0f, 0f, 0f);
+            transform.position = rinit;
+        }
+    }
+
+    void OnTriggerEnter(Collider target)
+    {
+        Destroy(target.gameObject);
     }
 }
---
+++ RollABall/Assets/Scripts/Rotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	void Update () {
+        transform.Rotate(new Vector3(10f, 10f, 10f) * Time.deltaTime);
+	}
+}
