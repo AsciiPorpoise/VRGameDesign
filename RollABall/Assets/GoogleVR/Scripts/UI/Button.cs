8b3a894c95a5e85bd80a125793f1f3500b8e7681
Mikel Matticoli <10008957@sbschools.org>
2016-11-23 07:41:29 -0600

    11/23

---
+++ RollABall/Assets/GoogleVR/Scripts/UI/Button.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour, IGvrGazeResponder {
+
+	// Use this for initialization
+	void OnGazeEnter()
+    {
+
+    }
+}
