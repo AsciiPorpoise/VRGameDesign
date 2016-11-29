7b2899a759ac9f9feef8ea7afb5a6a64c9712533
Mikel Matticoli <10008957@sbschools.org>
2016-11-29 08:09:20 -0600

    Main menu done. TODO: Level1 Camera Broken

--- RollABall/Assets/GoogleVR/Scripts/UI/Button.cs
+++ RollABall/Assets/GoogleVR/Scripts/UI/Button.cs
@@ -1,11 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
+using ProgressBar;
+using System.Linq;
 
 public class Button : MonoBehaviour, IGvrGazeResponder {
 
-	// Use this for initialization
-	void OnGazeEnter()
-    {
+    float timeSum = 0f;
+    bool looking = false;
 
+    public void OnGazeExit()
+    {
+        looking = false;
+    }
+
+    public void OnGazeTrigger()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    void IGvrGazeResponder.OnGazeEnter()
+    {
+        looking = true;
+    }
+
+    // Use this for initialization
+    void OnGazeEnter()
+    {
+    }
+
+    void Update()
+    {
+        if(looking)
+        {
+            timeSum += Time.deltaTime;
+        } else
+        {
+            timeSum = 0;
+        }
+
+        GameObject.FindGameObjectsWithTag("Loading")[0].GetComponent<TextMesh>().text = new String('●', ((int)(timeSum*10f)));
+
+        if (timeSum >= 2)
+        {
+            OnGazeTrigger();
+        }
     }
 }
