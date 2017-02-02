ff1f987ba5a584b16d381dc7b1e51f2717c80859
Mikel Matticoli <10008957@sbschools.org>
2017-02-02 08:11:00 -0600

    Created firebase test project, added sdk and set up database; started testing realtime data syncing to determine feasibikity of project idea

---
+++ FirebaseLab/Assets/Scripts/TextSync.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class TextSync : MonoBehaviour
+{
+
+    DatabaseReference tref;
+
+    // Use this for initialization
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://droidremote-6f969.firebaseio.com/");
+        tref = FirebaseDatabase.DefaultInstance.RootReference.Child("text");
+        tref.SetValueAsync("Hello World!");
+        tref.ValueChanged += HandleChange;
+    }
+
+    void HandleChange(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+        gameObject.GetComponent<TextMesh>().text = (string)args.Snapshot.Value;
+    }
+}
