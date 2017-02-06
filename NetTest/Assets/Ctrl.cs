5a670f079a7a3ab477a184ac6e0254b70fcbcef1
Mikel Matticoli <10008957@sbschools.org>
2017-02-06 08:09:39 -0600

    Testing different networking APIs (Firebase, NetworkManager Component

---
+++ NetTest/Assets/Ctrl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ctrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Input.gyro.attitude;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0.1f, 0f, 0f);
+        }
+    }
+}
