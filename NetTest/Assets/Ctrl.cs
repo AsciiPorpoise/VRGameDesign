b76d066202a11acd4036b9176e7dc1ff8381d46b
Mikel Matticoli <10008957@sbschools.org>
2017-02-21 07:48:46 -0600

    Finalized network API and began sensor input tests

--- NetTest/Assets/Ctrl.cs
+++ NetTest/Assets/Ctrl.cs
@@ -7,6 +7,7 @@ public class Ctrl : NetworkBehaviour {
 	// Use this for initialization
 	void Start () {
         Network.sendRate = 29;
+        Input.gyro.enabled = true;
 	}
 	
 	// Update is called once per frame
@@ -15,7 +16,12 @@ public class Ctrl : NetworkBehaviour {
         {
             return;
         }
-        transform.rotation = Input.gyro.attitude;
+
+        Debug.Log("Gyro: "+SystemInfo.supportsGyroscope);
+        Debug.Log("Gyro: "+Input.gyro.enabled);
+        Debug.Log(Input.gyro.attitude);
+
+        transform.rotation = Input.gyddadadro.attitude;//TODO: Map gyro by axis
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(-0.1f, 0f, 0f);
